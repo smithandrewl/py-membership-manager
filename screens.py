ee81bd57c7005bcf25c8c806c54528f05553530b
andrew <smith.andrew.l@gmail.com>
2021-01-13 19:36:58 -0600

    The main menu banner and options are rendered.

--- screens.py
+++ screens.py
@@ -12,4 +12,38 @@ class MainScreen:
         self.screen_manager = screen_manager
 
     def run(self):
-        print("Main Screen")
+        self.display_banner()
+        self.display_menu_prompt()
+
+    def display_banner(self):
+        print(
+            "\n".join(
+                [
+                    "                       _                   _     _              ",
+                    "  /\\/\\   ___ _ __ ___ | |__   ___ _ __ ___| |__ (_)_ __       ",
+                    " /    \\ / _ \\ '_ ` _ \\| '_ \\ / _ \\ '__/ __| '_ \\| | '_ \\ ",
+                    "/ /\\/\\ \\  __/ | | | | | |_) |  __/ |  \\__ \\ | | | | |_) |  ",
+                    "\\/    \\/\\___|_| |_| |_|_.__/ \\___|_|  |___/_| |_|_| .__/    ",
+                    "                                                  |_|           ",
+                    "                                                                ",
+                    "       /\\/\\   __ _ _ __   __ _  __ _  ___ _ __                ",
+                    "      /    \\ / _` | '_ \\ / _` |/ _` |/ _ \\ '__|              ",
+                    "     / /\\/\\ \\ (_| | | | | (_| | (_| |  __/ |                 ",
+                    "     \\/    \\/\\__,_|_| |_|\\__,_|\\__, |\\___|_|              ",
+                    "                           |___/ "
+                ]
+            )
+        )
+
+    def display_menu_prompt(self):
+        print(
+            "\n".join(
+                [
+                    "Welcome to club 'Unknown' Please enter a number:",
+                    "1. Administration",
+                    "2. Members",
+                    "3. Classes",
+                    "4. Exit"
+                ]
+            )
+        )
